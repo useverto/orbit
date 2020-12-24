7faab685cfb96a37f177e5580584d73e30a37101
John Letey <johnletey@gmail.com>
2020-12-24 10:30:17 +0000

    feat: start logic for order page

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,13 +1,51 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-import { tx } from "ar-gql";
+import { tx, run } from "ar-gql";
 import Head from "next/head";
-import { Page, Breadcrumbs, Spacer } from "@geist-ui/react";
+import {
+  Page,
+  Breadcrumbs,
+  Spacer,
+  Card,
+  Description,
+} from "@geist-ui/react";
+import { GQLNodeInterface } from "ar-gql/dist/types";
+import Arweave from "arweave";
+
+const getARAmount = (tx: GQLNodeInterface) => {
+  return `${parseFloat(tx.quantity.ar)} AR`;
+};
+
+const getPSTAmount = async (tx: GQLNodeInterface) => {
+  console.log(tx);
+  const contract = tx.tags.find((tag) => tag.name === "Contract");
+  const input = tx.tags.find((tag) => tag.name === "Input");
+  if (contract && input) {
+    const client = new Arweave({
+      host: "arweave.net",
+      port: 443,
+      protocol: "https",
+    });
+
+    const qty = JSON.parse(input.value).qty;
+    const res = await client.transactions.getData(contract.value, {
+      decode: true,
+      string: true,
+    });
+    const ticker = JSON.parse(res.toString()).ticker;
+
+    return `${qty} ${ticker}`;
+  }
+};
 
 const Order = () => {
   const router = useRouter();
   const [id, setID] = useState("");
   const [post, setPost] = useState("");
+  const [type, setType] = useState("");
+  const [value, setValue] = useState("");
+
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     if (router.query.id) {
@@ -15,8 +53,112 @@ const Order = () => {
       setID(router.query.id);
 
       // @ts-ignore
-      tx(router.query.id).then((tx) => {
+      tx(router.query.id).then(async (tx) => {
         setPost(tx.recipient);
+
+        const type = tx.tags.find((tag) => tag.name === "Type").value;
+        setType(type);
+
+        if (type === "Buy") {
+          setValue(getARAmount(tx));
+        }
+        if (type === "Sell") {
+          setValue(await getPSTAmount(tx));
+        }
+
+        if (type === "Buy") {
+          const res = await run(
+            `
+            query($post: String!, $order: [String!]!) {
+              transactions(
+                owners: [$post]
+                tags: [
+                  { name: "Exchange", values: "Verto" }
+                  { name: "Type", values: "PST-Transfer" }
+                  { name: "Order", values: $order }
+                ]
+                first: 1
+              ) {
+                edges {
+                  node {
+                    id
+                    tags {
+                      name
+                      value
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            { post: tx.recipient, order: router.query.id }
+          );
+
+          if (res.data.transactions.edges[0]) {
+            const amnt = await getPSTAmount(
+              res.data.transactions.edges[0].node
+            );
+            setOrders((orders) => {
+              return [
+                ...orders,
+                {
+                  title: res.data.transactions.edges[0].node.id,
+                  description: `PST Transfer - ${amnt}`,
+                },
+              ];
+            });
+          }
+        }
+        if (type === "Sell") {
+          // TODO(@johnletey): Query for an AR transfer
+        }
+
+        if (type === "Buy" || type === "Sell") {
+          const res = await run(
+            `
+            query($post: String!, $order: [String!]!) {
+              transactions(
+                owners: [$post]
+                tags: [
+                  { name: "Exchange", values: "Verto" }
+                  { name: "Type", values: "Confirmation" }
+                  { name: "Match", values: $order }
+                ]
+                first: 1
+              ) {
+                edges {
+                  node {
+                    id
+                    tags {
+                      name
+                      value
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            { post: tx.recipient, order: router.query.id }
+          );
+
+          if (res.data.transactions.edges[0]) {
+            setOrders((orders) => {
+              const received = res.data.transactions.edges[0].node.tags.find(
+                (tag) => tag.name === "Received"
+              ).value;
+              return [
+                ...orders,
+                {
+                  title: res.data.transactions.edges[0].node.id,
+                  description: `Confirmation`,
+                },
+              ];
+            });
+          }
+        }
+        if (type === "Swap") {
+          // TODO(@johnletey): Query for a swap confirmation
+        }
       });
     }
   }, [router.query.id]);
@@ -36,6 +178,18 @@ const Order = () => {
         </Breadcrumbs>
 
         <Spacer y={1} />
+
+        <Card width="50%">
+          <Description title={id} content={`${type} - ${value}`} />
+        </Card>
+        {orders.map((order) => (
+          <>
+            <Spacer y={1} />
+            <Card width="50%">
+              <Description title={order.title} content={order.description} />
+            </Card>
+          </>
+        ))}
       </Page>
     </>
   );
